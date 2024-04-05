e8d3b9568d4a7126e9e494a6d61afdbcb31397e7
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-05 11:20:26 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n ;
+    cin >> n;
+    printf(n >= 3000? "book" :
+            n >= 1000? "mask" :
+                        "no");
+    return 0;
+}
