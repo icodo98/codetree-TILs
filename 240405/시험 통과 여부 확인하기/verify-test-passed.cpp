280d7857c0d8906f99ba330c204ebfd98f844134
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-05 11:04:01 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    if(n >= 80) cout << "pass";
+    else printf("%d more score",80-n);
+    return 0;
+}
