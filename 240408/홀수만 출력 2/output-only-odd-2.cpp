42fbb6a9d92ea6dae852ee242dba8bf976d20055
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-08 08:09:14 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를작성해주세요.
+    int a, b;
+    cin >> b >> a;
+    for(int i = b; i >= a; i-=2){
+        cout << i << " ";
+    }
+    return 0;
+}
