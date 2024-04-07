080e84036ce711e9781cda1c48ffaef47f78e4af
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-08 07:50:04 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    printf(a % 3 == 0 ? "YES" : "NO");
+    printf("\n");
+    printf(a % 5 == 0 ? "YES" : "NO");
+
+
+    return 0;
+}
