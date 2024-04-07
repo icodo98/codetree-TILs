f4653ca34e0ba4241318ddea3b876d91fe019704
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-08 07:52:38 +0900

    Title: 4번의 크기 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d,e;
+    cin >> a>>b>>c>>d>>e;
+    printf(a > b ? "1\n" : "0\n");
+    printf(a > c ? "1\n" : "0\n");
+    printf(a > d ? "1\n" : "0\n");
+    printf(a > e ? "1\n" : "0\n");
+    
+    return 0;
+}
