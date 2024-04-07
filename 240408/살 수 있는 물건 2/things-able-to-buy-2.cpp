bdab66967ac4d58b26e93ab6395b40abb67b8790
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-08 07:47:08 +0900

    Title: 살 수 있는 물건 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    printf(n >= 3000 ? "book" :
+            n >= 1000 ? "mask" :
+                n >= 500 ? "pen" :
+                    "no");
+
+
+
+    return 0;
+}
