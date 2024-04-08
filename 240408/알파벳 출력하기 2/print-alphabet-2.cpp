6c2072ec7f4afdd0b6ffa4d0c57ef69820602f48
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-08 12:23:43 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240408/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int N;
+    cin >> N;
+    char c = 'A';
+    char z = 'Z';
+    int max = N;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N - max; j ++) cout << "  ";
+        for(int j = 0; j <max; j++){
+            cout << c << " ";
+            if(++c == z) c = 'A';
+        }
+        cout << endl;
+        max--;
+    }
+    return 0;
+}
