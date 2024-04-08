78e63e2d9ed0e7c421b1584adf9bcfad607bdfa5
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-08 13:17:14 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/나눗셈의 나머지/remainder-of-division.cpp
+++ 240408/나눗셈의 나머지/remainder-of-division.cpp
@@ -5,12 +5,13 @@ int main() {
     int a,b;
     cin >> a >> b;
     int arr[b] ={0};
-    while(a > 0){
+
+    while(a > 1){
         arr[a % b]++;
         a /= b;
     }
     int sum = 0;
-    for(int i = 0; i< b ;i++){
+    for(int i = 0; i < b ;i++){
         sum += arr[i] * arr[i];
     }
     cout << sum;
