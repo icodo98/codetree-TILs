91f961d06d2a733e66214b2b7e48620378281e9d
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-08 13:05:00 +0900

    Title: 전항의 두 배, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int arr[10];
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> arr[0] >> arr[1];
+    cout << arr[0] << " " << arr[1] << " ";
+    for(int i = 2; i < 10; i++){
+        arr[i] = arr[i-1] + 2 * arr[i-2];
+        cout <<arr[i] << " ";
+    }
+    return 0;
+}
