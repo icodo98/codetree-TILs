e48070ea9172124511e049ca0184e71f26c8fc80
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-09 08:28:52 +0900

    Title: 공백을 기준으로 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/공백을 기준으로 출력/output-based-on-space.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str1;
+    while(!cin.eof()){
+        cin >> str1;
+        cout << str1;
+    }
+    
+    return 0;
+}
