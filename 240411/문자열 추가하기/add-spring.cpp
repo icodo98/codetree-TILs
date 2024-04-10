b74d46ec87f3b2a671933a0dfa1e15bc4a7d375d
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-11 08:47:23 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240411/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string input;
+    cin >> input;
+    string hello = "Hello";
+    cout << input + hello;
+
+    return 0;
+}
