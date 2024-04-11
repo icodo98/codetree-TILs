28e0caca04a13f97fecae823bcd59bb55b434fcb
JUYOUNG LEE <102905512+icodo98@users.noreply.github.com>
2024-04-11 14:15:05 +0900

    Title: 알파벳 지우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240411/알파벳 지우기/remove-alphabet.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a,b;
+    cin >> a >> b;
+    string ans0, ans1;
+    ans0 = "";
+    ans1 = "";
+    for(int i = 0; i < a.length(); i++){
+        if(isalpha(a[i]) == 0) {
+            ans0 += a[i];
+        }
+        if(isalpha(b[i]) == 0) {
+            ans1 += b[i];
+        }
+    }
+    int n = stoi(ans0) + stoi(ans1);
+    cout << n;
+    return 0;
+}
